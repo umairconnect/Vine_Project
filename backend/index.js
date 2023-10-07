cb44655007b5af8dc3817a73e9a2974576dd992b
Umair Umer <moonsoftwizz@gmail.com>
2023-10-07 23:55:41 +0500

    login api with bcrypt package

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ const app = express();
 const cors = require('cors');
 const JWT_SEC = "my_jwt_sec";
 const connection = require('./db/config');
+const bcrypt = require('bcrypt');
 
 
 app.get("/", (req, resp) => {
@@ -17,27 +18,66 @@ app.use(express.json());
 app.use(cors());
 
 app.post('/signup', (req, res) => {
-
   const { fName, lName, user, email, password, experience, selectyourgoals, token } = req.body;
 
-  const insertQuery = 'INSERT INTO user (firstname, lastname, user, email, password, experience, selectyourgoals, token) VALUES (?, ?, ?, ?, ?, ?, ?, ?)';
-  const values = [fName, lName, user, email, password, experience, selectyourgoals, token];
-
-  connection.query(insertQuery, values, (err, results) => {
+  // Hash the password
+  bcrypt.hash(password, 10, (err, hashedPassword) => {
     if (err) {
-      console.error('Error inserting data:', err);
-      return;
+      console.error('Error hashing password:', err);
+      return res.status(500).json({ error: 'Error hashing password' });
     }
-    const token = jwt.sign({ user, email }, JWT_SEC, { expiresIn: '1h' });
-    res.status(200).json({ message: 'User registered successfully', token });
 
+    const insertQuery = 'INSERT INTO user (firstname, lastname, user, email, password, experience, selectyourgoals, token) VALUES (?, ?, ?, ?, ?, ?, ?, ?)';
+    const values = [fName, lName, user, email, hashedPassword, experience, selectyourgoals, token];
 
-    console.log('Data inserted successfully');
+    connection.query(insertQuery, values, (err, results) => {
+      if (err) {
+        console.error('Error inserting data:', err);
+        return res.status(500).json({ error: 'Error inserting data' });
+      }
+
+      const token = jwt.sign({ user, email }, JWT_SEC, { expiresIn: '1h' });
+      res.status(200).json({ message: 'User registered successfully', token, values });
+
+      console.log('Data inserted successfully');
+    });
   });
-
 });
 
 
+app.post('/login', (req, res) => {
+  const { email, password } = req.body;
 
+  const selectQuery = 'SELECT * FROM user WHERE email = ?';
+  connection.query(selectQuery, [email], (err, results) => {
+    if (err) {
+      console.error('Error querying database:', err);
+      return res.status(500).json({ error: 'Error querying database' });
+    }
+
+    if (results.length === 0) {
+      return res.status(401).json({ error: 'User not found' });
+    }
+
+    const user = results[0];
+
+    // Compare the provided password with the hashed password from the database
+    bcrypt.compare(password, user.password, (bcryptErr, result) => {
+      if (bcryptErr) {
+        console.error('Error comparing passwords:', bcryptErr);
+        return res.status(500).json({ error: 'Error comparing passwords' });
+      }
+
+      if (result) {
+        // Passwords match, generate a JWT token and send it as a response
+        const token = jwt.sign({ email: user.email }, JWT_SEC, { expiresIn: '1h' });
+        res.status(200).json({ message: 'Login successful', token });
+      } else {
+        // Passwords do not match
+        res.status(401).json({ error: 'Invalid password' });
+      }
+    });
+  });
+});
 
 app.listen(5000)
