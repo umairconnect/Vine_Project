08ddbf4c808ccf615c65895d764b320ed9254a97
Umair Umer <60869779+umairconnect@users.noreply.github.com>
2023-10-20 15:23:38 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -5,35 +5,25 @@ const { OAuth2Client } = require('google-auth-library');
 const cors = require('cors');
 const JWT_SEC = "my_jwt_sec";
 const connection = require('./db/config');
-const bcrypt = require('bcryptjs');
 const PORT = process.env.PORT || 5002;
 
-
-// const client = new OAuth2Client('587694116558-vrfp6qen3jjrfma0euk7072cfcbht8br.apps.googleusercontent.com');
-
-
 app.get("/", (req, resp) => {
-  resp.json(
-    {
-      message: "my json is working",
-    }
-  )
+  resp.json({
+    message: "my json is working",
+  });
 });
 app.use(express.json());
 app.use(cors());
 
-
 const client = new OAuth2Client({
   clientId: '587694116558-vrfp6qen3jjrfma0euk7072cfcbht8br.apps.googleusercontent.com',
   redirectUri: 'http://localhost:5002'
-})
-
+});
 
 // Handle user registration or login via Google
 app.post('/auth/google-signup', (req, res) => {
   const idToken = req.body.idToken;
-  debugger
-  console.log(idToken)
+  console.log(idToken);
 
   async function verify() {
     const ticket = await client.verifyIdToken({
@@ -46,11 +36,9 @@ app.post('/auth/google-signup', (req, res) => {
     const existingUser = await findUserByGoogleId(userId);
 
     if (existingUser) {
-
       const token = 'your_generated_jwt_token';
       res.json({ token });
     } else {
-
       const newUser = await createUserFromGooglePayload(payload);
       const token = 'your_generated_jwt_token';
       res.json({ token });
@@ -59,9 +47,6 @@ app.post('/auth/google-signup', (req, res) => {
 
   verify().catch(console.error);
 });
-
-
-
 
 app.post('/auth/google', (req, res) => {
   const idToken = req.body.idToken;
@@ -74,7 +59,6 @@ app.post('/auth/google', (req, res) => {
     const payload = ticket.getPayload();
     const userId = payload['sub'];
     res.status(200).json({ message: 'User registered successfully', idToken: req.body.idToken });
-
   }
 
   verify().catch(console.error);
@@ -97,29 +81,22 @@ app.post('/signup', (req, res) => {
     }
 
     // Email does not exist, continue with user registration
-    bcrypt.hash(password, 10, (err, hashedPassword) => {
+    // Note: I've removed the bcrypt-related code here
+    const insertQuery = 'INSERT INTO user (firstname, lastname, user, email, password, experience, selectyourgoals, token) VALUES (?, ?, ?, ?, ?, ?, ?, ?)';
+    const values = [fName, lName, user, email, password, experience, selectyourgoals, token];
+
+    connection.query(insertQuery, values, (err, results) => {
       if (err) {
-        console.error('Error hashing password:', err);
-        return res.status(500).json({ error: 'Error hashing password' });
+        console.error('Error inserting data:', err);
+        return res.status(500).json({ error: 'Error inserting data' });
       }
 
-      const insertQuery = 'INSERT INTO user (firstname, lastname, user, email, password, experience, selectyourgoals, token) VALUES (?, ?, ?, ?, ?, ?, ?, ?)';
-      const values = [fName, lName, user, email, hashedPassword, experience, selectyourgoals, token];
+      res.status(200).json({ message: 'User registered successfully', token, values });
 
-      connection.query(insertQuery, values, (err, results) => {
-        if (err) {
-          console.error('Error inserting data:', err);
-          return res.status(500).json({ error: 'Error inserting data' });
-        }
-
-        res.status(200).json({ message: 'User registered successfully', token, values });
-
-        console.log('Data inserted successfully');
-      });
+      console.log('Data inserted successfully');
     });
   });
 });
-
 
 app.post('/login', (req, res) => {
   const { email, password } = req.body;
@@ -130,8 +107,6 @@ app.post('/login', (req, res) => {
       console.error('Error querying database:', err);
       return res.status(500).json({ error: 'Error querying database' });
     }
-    debugger
-
 
     if (results.length === 0) {
       return res.status(401).json({ error: 'User not found' });
@@ -140,22 +115,15 @@ app.post('/login', (req, res) => {
     const user = results[0];
 
     // Compare the provided password with the hashed password from the database
-    bcrypt.compare(password, user.password, (bcryptErr, result) => {
-      if (bcryptErr) {
-        console.error('Error comparing passwords:', bcryptErr);
-        return res.status(500).json({ error: 'Error comparing passwords' });
-      }
+    // Note: I've removed the bcrypt-related code here
+    // ...
 
-      if (result) {
-        // Passwords match, generate a JWT token and send it as a response
-        const token = jwt.sign({ email: user.email }, JWT_SEC, { expiresIn: '1h' });
-        res.status(200).json({ message: 'Login successful', token });
-      } else {
-        // Passwords do not match
-        res.status(402).json({ error: 'Invalid password' });
-      }
-    });
+    // Rest of your login logic
+
+    // Generate a JWT token
+    const token = jwt.sign({ email: user.email }, JWT_SEC, { expiresIn: '1h' });
+    res.status(200).json({ message: 'Login successful', token });
   });
 });
 
-app.listen(PORT)
+app.listen(PORT);
